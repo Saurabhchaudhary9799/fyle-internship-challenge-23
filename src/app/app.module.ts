6b1de4a83eb7650e0466c51ebb1a0c4ec386f916
Saurabh Chaudhary <Saurbhbailara@gmail.com>
2024-05-10 19:41:37 +0530

    static design task finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from  '@angular/common/http';
+import { UserSearchComponent } from './user-search/user-search.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { UserReposComponent } from './user-repos/user-repos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserSearchComponent,
+    UserProfileComponent,
+    UserReposComponent
   ],
   imports: [
     BrowserModule,
